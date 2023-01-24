ad6931b766ccbc5f101a7c68b033d74088ec8051
HuseyinMammadli <96258053+HuseyinMammadli@users.noreply.github.com>
2023-01-24 12:42:59 +0400

    second

---
+++ Consumer/RabbitMQService.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+
+namespace RabbitMQSample
+{
+    public class RabbitMQService
+    {
+
+        private readonly string _hostName = "192.168.13.129";
+
+        public IConnection GetRabbitMQConnection()
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                
+                HostName = _hostName,
+                Port = 5672,
+                UserName = "huseyn",
+                Password = "123456"
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RabbitMQSample
+{
+    class Program
+    {
+        private static readonly string _queueName = "Huseyn-1";
+        private static Publisher _publisher;
+
+        static void Main(string[] args)
+        {
+            _publisher = new Publisher(_queueName, "Salam");
+
+            Console.ReadKey();
+        }
+    }
+}
